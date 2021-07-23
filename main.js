261d06937a5df54fc94baaef0aca49af266dd933
Andrew <73196050+XJloTT4eH9l@users.noreply.github.com>
2021-07-23 23:25:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.testimonials__slider').slick({
+      infinite: true,
+		dots: true,
+	  	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	arrows: false,
+	  	autoplay: true,
+	  	autoplaySpeed: 3000,
+	  	speed: 2000
+   });
+});
